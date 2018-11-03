de19b8b292aa322e90b873862f6346683a251bf9
Andreas Mandel <andreas.mandel@gmail.com>
2018-11-03 19:18:34 +0100

    Do not run update if there is already one running.

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+pgrep java && echo "INFO: Update already running." && exit 1
 touch /dependencycheck/update.log
-(cd /dependencycheck && ./gradlew --no-daemon -b database.gradle  update >>/dependencycheck/update.log 2>&1) || echo "ERROR: update failed."
+(cd /dependencycheck && ./gradlew --no-daemon -b database.gradle update >>/dependencycheck/update.log 2>&1) || echo "ERROR: update failed."
